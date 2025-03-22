df45e00c5af0752f712d52c18247e0ab1a5a1eaa
Gabriel de Souza <gabrielsouza080802@gmail.com>
2025-03-22 10:52:23 -0300

    Quarto Commit

--- calculadorademedia/src/main/java/calculadorademedia/Main.java
+++ calculadorademedia/src/main/java/calculadorademedia/Main.java
@@ -5,8 +5,26 @@ public class Main {
         int contador = 0;
         int media;
         int soma = 0;
-
+        int flag = 0;
+        int nota;
         Scanner input = new Scanner(System.in);
         
+        while (flag >= 0)
+        {
+            System.out.println("Digite a nota, ou digite um número negativo para encerrar: ");
+            nota =  input.nextInt();
+            if (nota < 0)
+            {
+                flag = -1;
+                input.close();
+            }
+            else {
+                soma += nota;
+                contador++;
+            }
+        }
+        media = soma/contador;
+        System.out.println("Número de dados inseridos: " + contador + " média dos dados: " + media);
+
     }
 }

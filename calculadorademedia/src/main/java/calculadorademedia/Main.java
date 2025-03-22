ed65861a83775fcd81a05b7778f02ec101fcfec6
Gabriel de Souza <gabrielsouza080802@gmail.com>
2025-03-22 10:46:11 -0300

    Terceiro Commit

--- calculadorademedia/src/main/java/calculadorademedia/Main.java
+++ calculadorademedia/src/main/java/calculadorademedia/Main.java
@@ -1,9 +1,12 @@
 package calculadorademedia;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int contador = 0;
         int media;
         int soma = 0;
+
+        Scanner input = new Scanner(System.in);
+        
     }
 }
